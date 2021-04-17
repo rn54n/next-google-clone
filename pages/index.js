63d145f2e1d14437c04bf86b97f9d77f3f3ec669
Aaren San <info@aarensan.com>
2021-04-17 18:42:18 +0200

    test: add buttons and custom css

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
       </header>
 
       {/* Body */}
-      <form>
+      <form className="flex flex-col items-center mt-44 flex-grow">
         <Image
           width="272"
           height="92"
@@ -43,6 +43,11 @@ export default function Home() {
           <SearchIcon className="h-5 mr-3 text-gray-500" />
           <input type="text" className="flex-grow focus:outline-none" />
           <MicrophoneIcon className="h-5" />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
 
