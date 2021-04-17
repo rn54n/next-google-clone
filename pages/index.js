6e0ce1c9e8b1c8b8b90f514dfa51a0db327cf155
Aaren San <info@aarensan.com>
2021-04-17 18:28:22 +0200

    test: add input field and icons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
 import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
 export default function Home() {
@@ -38,6 +39,11 @@ export default function Home() {
           height="92"
           src="/assets/images/googlelogo_color_272x92dp.png"
         />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
       </form>
 
       {/* Footer */}
